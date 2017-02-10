958476e991b0ec9c3265e8cd78ad4580403a7c05
zhan1803 <zhan1803@purdue.edu>
2017-02-09 19:06:11 -0500

    deleted CustomTimer.java file, will not be used for timer

--- app/src/main/java/com/example/guessit/guessit/MainActivity.java
+++ app/src/main/java/com/example/guessit/guessit/MainActivity.java
@@ -70,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         timerStart.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //startActivity(new Intent(MainActivity.this, CustomTimer.class));
                 countDownTimer.start();
             }
         });
